a6402929e53e2751d95b2a13b3b382a595f0b366
pisti2000 <nemist186@hengersor.hu>
2025-02-27 09:15:33 +0100

    get hiba javítása
    app.get("/", (req, res) => {
    
        res.send("müködik a szerver")
    })

--- teliolimpia.js
+++ teliolimpia.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const bodyparser = reqire("body-parser");
+const bodyparser = require("body-parser");
 
 
 app.use(bodyparser.json());
 
+app.get("/", (req, res) => {
 
+    res.send("müködik a szerver")
+})
 
 
 app.listen(3000, () =>{
-    
+
     console.log("A teliolimpia szervere a 3000-es porton fut.")
 })
