f10309e49574cfa23abebfa5558ea20155e013f6
pisti2000 <nemist186@hengersor.hu>
2025-02-27 09:25:57 +0100

    db kapcsolat létrehozása

--- teliolimpia.js
+++ teliolimpia.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const app = express();
 const bodyparser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql");
 
 
+
+app.use(cors());
 app.use(bodyparser.json());
+
+const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        port:3307,
+        password:"",
+        database:"teliolimpia"
+
+})
+
+
 
 app.get("/", (req, res) => {
 
